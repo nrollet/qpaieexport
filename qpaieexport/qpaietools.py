2b806ea0a4010ec6fac95299a399581925af1074
Nicolas ROLLET <nicolas@axe.lan>
2019-03-03 20:21:50 +0100

    qpaie

--- qpaieexport/qpaietools.py
+++ qpaieexport/qpaietools.py
@@ -12,8 +12,12 @@ class QueryPaie(object):
 
         self.conx = ""
         self.mdbpath = ""
+        self.param = {}
         self.periodepaie = ""
-        self.raisonsoc = ""
+        self.databull = []
+        self.dataemp = []
+
+        # self.raisonsoc = ""
 
     def connect(self, mdbpath):
 
@@ -32,16 +36,77 @@ class QueryPaie(object):
         self.conx.commit()
         self.conx.close()
 
+    # def periodepaie(self):
+    # sql = """SELECT PeriodePaie FROM ConstantesEntreprise"""
+    # self.cursor.execute(sql)
+    # self.periodepaie = self.cursor.fetchall()[0]
+
     def param_dossier(self):
-        """Paramètres des établissements"""
-        sql = """
-            SELECT CodeEtablissement, RaisonSocial, PeriodePaie 
-            FROM Etablissements
-        """
+        """requête pour les paramètres des établissements"""
+        sql = "SELECT CodeEtablissement, RaisonSociale FROM Etablissements"
         self.cursor.execute(sql)
-        return self.cursor.fetchall()
+        for etab, rs in self.cursor.fetchall():
+            self.param.update({"etab": {"rs": rs, "code": etab}})
 
+        sql = """SELECT PeriodePaie FROM ConstantesEntreprise"""
+        self.cursor.execute(sql)
+        self.periodepaie = self.cursor.fetchall()[0][0]
+        # self.param.update({"periode": self.cursor.fetchall()[0]})
 
+        return self.param
+
+    def employes(self):
+        sql = """
+            SELECT EM.Numero, EM.NomNaissance, 
+            EM.NomMarital, EM.Prenom, 
+            EM.DateEntree1, EM.DateEntree2,
+            EM.DateSortie1, EM.DateSortie2
+            FROM Employes EM 
+            INNER JOIN CriteresLibres CL
+            ON EM.Numero=CL.NumeroEmploye
+            """
+
+        self.cursor.execute(sql)
+        data = self.cursor.fetchall()
+        for (
+            Numero,
+            NomNaissance,
+            NomMarital,
+            Prenom,
+            DateEntree1,
+            DateEntree2,
+            DateSortie1,
+            DateSortie2,
+        ) in data:
+            numero = Numero.lstrip()
+            nom = NomNaissance
+            if NomMarital:
+                nom = NomMarital
+            nom = nom + " " + Prenom
+            entree = DateEntree1
+            if DateEntree2 > DateEntree1:
+                entree = DateEntree2
+            sortie = DateSortie1
+            if DateSortie1 and DateEntree2 == "":
+                sortie = DateSortie1
+            elif DateSortie2:
+                sortie = DateSortie2
+            if entree == datetime(1899, 12, 30, 0, 0):
+                entree = None
+            if sortie == datetime(1899, 12, 30, 0, 0):
+                sortie = None
+
+            self.dataemp.append([numero, nom, entree, sortie])
+
+        return self.dataemp
+
+    def collecte_bulletins(self):
+        """requête pour les données des bulletins"""
+        sql = "SELECT * FROM Bulletins"
+
+        self.cursor.execute(sql)
+        self.databull = self.cursor.fetchall()
+        return self.databull
 
 
 if __name__ == "__main__":
@@ -59,5 +124,8 @@ if __name__ == "__main__":
     if o.connect(mdbpath):
         logging.info("success")
     pp.pprint(o.param_dossier())
+    # pp.pprint(o.employes())
+    pp.pprint(o.periodepaie)
+    pp.pprint(o.collecte_bulletins())
     o.disconnect()
 
---
+++ tests/queries.sql
@@ -0,0 +1,19 @@
+SELECT EM.Numero, EM.NomNaissance, EM.NomMarital, EM.Prenom, 
+FROM Employes EM INNER JOIN CriteresLibres CL
+    ON EM.Numero=CL.NumeroEmploye
+WHERE
+((EM.DateSortie1=0 OR EM.DateSortie1>#2/01/2019
+#) OR
+(EM.DateEntree2<>0 AND
+(EM.DateSortie2>#2/01/2019# OR
+EM.DateSortie2=0)))
+
+SELECT NumeroEmploye, periode, SBase, Brut, TrancheA,
+    TrancheB, TrancheC, BaseSecu,
+    TotRetenue, TotCotisPatron,
+    NetImpos, NetAPayer, MtNetPayeTheo,
+    NbHNormal, NbHTrav, CoutGlobalMois,
+    CumCpDus, CumCpPris, CumProvCp,
+    CumMtCpPris, CumCpDus_1, CumCpPris_1,
+    CumProvCp_1, CumMtCpPris_1
+FROM Bulletins
